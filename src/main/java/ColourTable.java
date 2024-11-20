7c086924f0fc399200c6c34385395aa36ffaa2c6
JosephB03 <brennanjoe909@gmail.com>
2024-11-20 11:53:17 +0000

    Added unit test to test constructor input is greater than 0 and a power of 2 for ColourTable

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -2,6 +2,10 @@ public class ColourTable {
     private final int paletteSize;
 
     public ColourTable(int paletteSize) {
+        // check that value is greater than 0 and bitwise AND to check power of 2
+        if (paletteSize < 1 || (paletteSize & (paletteSize-1)) != 0) {
+            throw new IllegalArgumentException("Palette size must be greater than 0 and a power of 2");
+        }
         this.paletteSize = paletteSize;
     }
 
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -5,6 +5,12 @@ public class ColourTableTest {
     @Test
     void testConstructorInput() {
         ColourTable table = new ColourTable(4);
-        assertEquals(4, table.getPaletteSize(), "Should match input in declaration");
+        assertEquals(4, table.getPaletteSize(), "Should match input in declaration of 4");
     }
+    @Test
+    void testPowerOfTwoInput() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(-1), "Palette size must be greater than 0");
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(3), "Palette size must be a power of 2. 3 is invalid");
+    }
+
 }
