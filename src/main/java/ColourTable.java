784084b7504cb0dec581a0ff074e92b02efe1153
JosephB03 <brennanjoe909@gmail.com>
2024-11-20 16:03:01 +0000

    Improved comments throughout code and removed redundant imports.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,10 +1,9 @@
-import javax.naming.SizeLimitExceededException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ColourTable {
-    private final int paletteSize;
-    private final List<Integer> colourList = new ArrayList<>();
+    private final int paletteSize; // number of colours in palette
+    private final List<Integer> colourList = new ArrayList<>(); // list of colours for palette
 
     public ColourTable(int paletteSize) {
         // check that value is greater than 0 and bitwise AND to check power of 2
@@ -17,12 +16,13 @@ public class ColourTable {
     public int getPaletteSize() {
         return this.paletteSize;
     }
+    // Add a new colour with an RGB value
     public void add(Integer RGBValue) {
-        if (RGBValue > 0xFFFFFF) {
+        if (RGBValue > 0xFFFFFF) { // If RGBValue > 24 bits
             throw new IllegalArgumentException("RGB value cannot exceed 24 bits");
         } else if (RGBValue < 0) {
             throw new IllegalArgumentException("RGB value must be positive");
-        } else if (colourList.size() == paletteSize) {
+        } else if (colourList.size() == paletteSize) { // if palette is full
             throw new IllegalStateException("Colour table is full");
         }
         colourList.add(RGBValue);
